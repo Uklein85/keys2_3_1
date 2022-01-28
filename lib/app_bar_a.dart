b110da5b51f20f263e33124917578edc009cee86
Uklein85 <97888872+Uklein85@users.noreply.github.com>
2022-01-28 11:01:51 +0700

    Initial commit

---
+++ lib/app_bar_a.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class AppBarA extends StatefulWidget {
+  const AppBarA({Key? key}) : super(key: key);
+
+  @override
+  _AppBarAState createState() => _AppBarAState();
+}
+
+class _AppBarAState extends State<AppBarA> {
+
+  @override
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //home:const
+      initialRoute: '/',
+      routes: {
+        '/': (context)=> const MainScreen(),
+        'contacts': (context)=>SecondScreen(),
+        "plugins":(context)=>TherdScreen(),
+      },
+
+    );
+
+  }
+}
+Widget navDrawer( context)=>Drawer(
+  child: ListView(padding: EdgeInsets.zero,children: [
+    DrawerHeader (decoration: const BoxDecoration(color:Colors.blue ),
+
+        child: SizedBox(
+          height: 200,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container (height: 90,
+                decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(55.0) )),
+
+                child: Image.network ('https://www.agiratech.com/wp-content/uploads/2020/08/flutter.png'),
+              ),
+
+              Text('Навигация во Flutter')
+            ],
+          ),
+        )
+    ),
+    ListTile (
+      leading: Icon(Icons.one_k),
+      title: Text('Контакты'),
+      onTap: (){Navigator.pushNamed(context, '/');
+
+      },
+    ),
+    ListTile (
+      leading: Icon(Icons.two_k),
+      title: Text('Каталог'),
+      onTap: (){Navigator.pushNamed(context, 'contacts');
+
+      },
+    ),
+    Divider(),
+    const Padding(
+      padding: EdgeInsets.only(left:140),
+      child: Text('Меню настроек'),
+
+    ),
+    ListTile (
+      leading: Icon(Icons.settings),
+      title: Text('настройки'),
+      onTap: (){Navigator.pushNamed(context, 'plugins');
+      },
+    ),
+  ],),
+);
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super (key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar (title: Text('Контакты')),
+      drawer: navDrawer(context),
+      body: const Center(
+        child: Text('Контакты'),
+      ),
+    );
+  }
+}
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super (key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Каталог')),
+      drawer: navDrawer(context),
+      body: const Center(
+        child: Text('Каталог'),
+      ),
+    );
+  }
+}
+class TherdScreen extends StatelessWidget {
+  const TherdScreen({Key? key}) : super (key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar (title: Text('Меню настроек')),
+      drawer: navDrawer(context),
+      body: const Center(
+        child: Text('Меню настроек'),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:keys2_3_1/app_bar_a.dart';
+
+void main() {
+  runApp(const AppBarA());
+
+}
+
+
